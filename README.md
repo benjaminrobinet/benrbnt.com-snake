864a7df45d11708bdc8126242dfa1460a23edddc
Benjamin Robinet <benjaminrobinet@outlook.fr>
2020-09-24 16:05:42 +0200

    Update

---
+++ README.md
@@ -0,0 +1,5 @@
+# benrbnt.com - snake
+Pending screen while developing a portfolio.
+
+# Run
+This project si bundled using parceljs. Example: `parcel index.html`
---
+++ src/Snake/Viewport.js
@@ -0,0 +1,31 @@
+export class Viewport {
+    constructor(core) {
+        this.toLayout = new Set();
+        this.attach()
+    }
+
+    get width() {
+        return window.innerWidth;
+    }
+
+    get height() {
+        return window.innerHeight;
+    }
+
+    onResize = () => {
+        this.toLayout.forEach(fn => fn());
+    }
+
+
+    layout(fn) {
+        this.toLayout.add(fn);
+
+        return () => {
+            this.toLayout.delete(fn);
+        }
+    }
+
+    attach() {
+        window.addEventListener('resize', this.onResize);
+    }
+}
